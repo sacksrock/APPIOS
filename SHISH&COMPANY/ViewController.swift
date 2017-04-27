69ce0be96ffefe55c41ab7277a5afb122fa2c6c7
daniel de la iglesia <gradomedio2smr@gmail.com>
2017-04-27 11:27:29 +0200

    Initial Commit

---
+++ SHISH&COMPANY/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SHISH&COMPANY
+//
+//  Created by Daniel de la Iglesia Gonzalez on 27/4/17.
+//  Copyright © 2017 Daniel de la Iglesia, Alejandro Martín. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SHISH&COMPANYTests/SHISH_COMPANYTests.swift
@@ -0,0 +1,36 @@
+//
+//  SHISH_COMPANYTests.swift
+//  SHISH&COMPANYTests
+//
+//  Created by Daniel de la Iglesia Gonzalez on 27/4/17.
+//  Copyright © 2017 Daniel de la Iglesia, Alejandro Martín. All rights reserved.
+//
+
+import XCTest
+@testable import SHISH_COMPANY
+
+class SHISH_COMPANYTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
